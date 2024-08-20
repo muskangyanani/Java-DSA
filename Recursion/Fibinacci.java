2931a78fd977882b67112baae0a7bd6a72c0cd1f
muskangyanani <muskaann.129@gmail.com>
2024-08-20 22:24:35 +0530

    Fibonacci by formula

--- Recursion/Fibinacci.java
+++ Recursion/Fibinacci.java
@@ -2,16 +2,27 @@ package Recursion;
 
 public class Fibinacci {
     public static void main(String[] args) {
-        System.out.println(fibinacci(4));
+        // for input 50 or any larger value this will not give any output because its
+        // time complexiti is exponential.
+        // System.out.println(fiboExponentialTime(50));
+        // for (int i = 0; i < 20; i++) {
+        // System.out.println(fiboFormula(i));
+        // }
+
+        System.out.println(fiboFormula(50));
     }
 
-    static int fibinacci(int n) {
+    static int fiboFormula(int n) {
+        return (int) (Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
+    }
+
+    static int fiboExponentialTime(int n) {
 
         // base condition
         if (n < 2) {
             return n;
         }
 
-        return fibinacci(n - 1) + fibinacci(n - 2);
+        return fiboExponentialTime(n - 1) + fiboExponentialTime(n - 2);
     }
 }
